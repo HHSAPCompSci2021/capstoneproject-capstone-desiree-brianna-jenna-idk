227329118d66d2d002beb59e45d338d225dad3c0
Jenna Wang <65796046+ariyin@users.noreply.github.com>
2022-05-20 23:58:04 -0700

    clean up

--- CapstoneProject/src/NamCap.java
+++ CapstoneProject/src/NamCap.java
@@ -16,7 +16,7 @@ public class NamCap {
 	
 	private boolean hasKiwi;
 	private PImage image;	
-	private int x, y, xi, yi, direction, hkCount, nkCount;
+	private int x, y, xi, yi, direction;
 	private ArrayList<Coordinate> fruitLoc, playerLoc;
 	//private ArrayList<Point> fruitLoc, playerLoc;
 	
@@ -69,26 +69,36 @@ public class NamCap {
 	 */
 	public void act(Map grid, Player player)
 	{	
+		// bfs
 		if(!hasKiwi)
 		{
-			ArrayList<Point> fruitLoc = find('k', player, grid);
-			if(fruitLoc != null && fruitLoc.size() > 1)
-			{
-				x = (int) fruitLoc.get(fruitLoc.size() - 2).getY() * 30 + 15;
-				y = (int) fruitLoc.get(fruitLoc.size() - 2).getX() * 30 + 15;
-			}
+			Coordinate p = getKiwiPath(y / 30, x / 30, grid);
+			while(p.getParent() != null) {
+				//System.out.println("here");
+				 fruitLoc.add(p);
+				 p = p.getParent();
+		     }
+			
+			//System.out.println(fruitLoc)
+			 x = fruitLoc.get(fruitLoc.size() - 1).y * 30 + 15;
+			 y = fruitLoc.get(fruitLoc.size() - 1).x * 30 + 15;
 		}
-		
+
 		else if(hasKiwi)
 		{
-			ArrayList<Point> playerLoc = findLoc(player.getX(), player.getY(), grid);
-			if(playerLoc != null && playerLoc.size() > 1)
-			{
-				x = (int) playerLoc.get(playerLoc.size() - 2).getY() * 30 + 15;
-				y = (int) playerLoc.get(playerLoc.size() - 2).getX() * 30 + 15;
-			}
+			 
+			 Coordinate p2 = getPlayerPath(x / 30, y / 30, grid, player);
+			 while(p2.getParent() != null)
+			 {
+				 System.out.println(playerLoc);
+				 playerLoc.add(p2);
+				 p2 = p2.getParent();
+			 }
+			 
+			 
+			 x = playerLoc.get(playerLoc.size() - 1).y * 30 + 15;
+			 y = playerLoc.get(playerLoc.size() - 1).x * 30 + 15;
 		}
-		
 	}
 	
 	/**
@@ -160,7 +170,6 @@ public class NamCap {
 		if(f instanceof Kiwi)
 		{
 			hasKiwi = true;
-			hkCount = 0;
 		}
 	}
 	
@@ -169,249 +178,8 @@ public class NamCap {
 	 */
 	public void setKiwiFalse() {
 		hasKiwi=false;
-		nkCount = 0;
-	}
-	
-	/**
-	 * Finds a character in the Map in the smallest number of moves.
-	 * 
-	 * @param target The target character.
-	 * @param player The Player in the map.
-	 * @param grid The Map of the game.
-	 * @return An ArrayList of Points for the next moves naM-caP should make.
-	 */
-	public ArrayList<Point> find(char target, Player player, Map grid) {
-		return(find(y / 30, x / 30, target, player, grid));
-	}
-	
-	/**
-	 * Finds a location in the Map's grid in the smallest number of moves.
-	 * 
-	 * @param i The row of the Map.
-	 * @param j The column of the Map.
-	 * @param grid The Map of the game.
-	 * @return An ArrayList of Points for the next moves naM-caP should make.
-	 */
-	public ArrayList<Point> findLoc(int i, int j, Map grid)
-	{
-		return(findLoc(y / 30, x / 30, i / 30, j / 30, grid));
 	}
 
-	private ArrayList<Point> find(int i, int j, char target, Player player, Map grid)
-	{	
-		// BASE CASES
-		// Are you out of the grid bounds?
-		if(i >= grid.getLength() || i < 0 || j > grid.getRowLength(i) || j < 0)
-		{
-			return null;
-		}
-
-		//System.out.println("i: " + i + ", j: " + j + ", (i, j): " + grid.get(i, j));
-		// Are you in a wall? Are you somewhere you have been before? Is there a player in the way?
-		if(grid.get(i, j) == '#' || grid.get(i, j) == '!' || (i == player.getX() / 30 && j == player.getY() / 30) || grid.get(i, j) == '*')
-		{
-			//System.out.println("here1");
-			return null;
-		}
-
-		// Are you at the exit?
-		if(grid.get(i, j) == target)
-		{
-			ArrayList<Point> path = new ArrayList<Point>();
-			path.add(new Point(i, j));
-			return path;
-		}
-				
-		// RECURSIVE CASE
-		else
-		{
-			// Save the character at grid location x,y in a local variable for later use
-			char location = grid.get(i, j);
-			grid.set(i, j, '!');
-			
-			// Recursively call findPath() 4 times - once in each of the 4 fundamental directions (one space up, down, left, and right). Save the ArrayList that is returned by each.
-			// Of the 4 ArrayLists that are returned, find the ArrayList that is not null and has the smallest size.
-			
-			ArrayList<Point> one = find(i, j - 1, target, player, grid);
-			ArrayList<Point> two = find(i - 1, j, target, player, grid);
-			ArrayList<Point> three = find(i, j + 1, target, player, grid);
-			ArrayList<Point> four = find(i + 1, j, target, player, grid);
-			
-			ArrayList<Point> min = null;
-			
-			if(one != null)
-			{
-				min = one;
-			}
-			
-			else if(two != null)
-			{
-				min = two;
-			}
-			
-			else if(three != null)
-			{
-				min = three;
-			}
-			
-			else if(four != null)
-			{
-				min = four;
-			}
-			
-			if(two != null)
-			{
-				if(two.size() < min.size())
-				{
-					min = two;
-				}
-			}
-			
-			if(three != null)
-			{
-				if(three.size() < min.size())
-				{
-					min = three;
-				}
-			}
-			
-			if(four != null)
-			{
-				if(four.size() < min.size())
-				{
-					min = four;
-				}
-			}
-
-			// Put the original saved character back into the grid at x,y (to remove the breadcrumb and prevent any permanent modification to the grid)
-			grid.set(i, j, location);
-			
-			// If all 4 of the ArrayLists returned by the recursive calls are null:
-			if(one == null && two == null && three == null && four == null)
-			{
-				return null;
-			}
-			
-			// If you have found the smallest non-null ArrayList:
-			if(min.size() != 0)
-			{
-				// Add this location to the beginning of the list, then return it.
-				min.add(new Point(i, j)); 
-				return min;
-			}
-		}
-						
-		return null;
-	}
-	
-	// x and y are namcap's coordinates
-	// i and j are the target coordinates
-	private ArrayList<Point> findLoc(int x, int y, int i, int j, Map grid)
-	{	
-		//System.out.println("x: " + x + ", y: " + y + ", i: " + i + ", j: " + j);
-		// BASE CASES
-		// Are you out of the grid bounds?
-		if(x >= grid.getLength() || x < 0 || y > grid.getRowLength(x) || y < 0)
-		{
-			return null;
-		}
-
-		// Are you in a wall? Are you somewhere you have been before? Are you at the starting box?
-		if(grid.get(x, y) == '#' || grid.get(x, y) == '!' || grid.get(x, y) == '*')
-		{
-			return null;
-		}
-
-		// Are you at the exit?
-		if(x == i && y == j)
-		{
-			ArrayList<Point> path = new ArrayList<Point>();
-			path.add(new Point(x, y));
-			return path;
-		}
-				
-		// RECURSIVE CASE
-		else
-		{
-			// Save the character at grid location x,y in a local variable for later use
-			char location = grid.get(x, y);
-			grid.set(x, y, '!');
-			
-			// Recursively call findPath() 4 times - once in each of the 4 fundamental directions (one space up, down, left, and right). Save the ArrayList that is returned by each.
-			// Of the 4 ArrayLists that are returned, find the ArrayList that is not null and has the smallest size.
-			
-			ArrayList<Point> one = findLoc(x, y - 1, i, j, grid);
-			ArrayList<Point> two = findLoc(x - 1, y, i, j, grid);
-			ArrayList<Point> three = findLoc(x, y + 1, i, j, grid);
-			ArrayList<Point> four = findLoc(x + 1, y, i, j, grid);
-			
-			ArrayList<Point> min = null;
-			
-			if(one != null)
-			{
-				min = one;
-			}
-			
-			else if(two != null)
-			{
-				min = two;
-			}
-			
-			else if(three != null)
-			{
-				min = three;
-			}
-			
-			else if(four != null)
-			{
-				min = four;
-			}
-			
-			if(two != null)
-			{
-				if(two.size() < min.size())
-				{
-					min = two;
-				}
-			}
-			
-			if(three != null)
-			{
-				if(three.size() < min.size())
-				{
-					min = three;
-				}
-			}
-			
-			if(four != null)
-			{
-				if(four.size() < min.size())
-				{
-					min = four;
-				}
-			}
-
-			// Put the original saved character back into the grid at x,y (to remove the breadcrumb and prevent any permanent modification to the grid)
-			grid.set(x, y, location);
-			
-			// If all 4 of the ArrayLists returned by the recursive calls are null:
-			if(one == null && two == null && three == null && four == null)
-			{
-				return null;
-			}
-			
-			// If you have found the smallest non-null ArrayList:
-			if(min.size() != 0)
-			{
-				// Add this location to the beginning of the list, then return it.
-				min.add(new Point(x, y)); 
-				return min;
-			}
-		}
-						
-		return null;
-	}
-	
 	/**
 	 * Determines if the Fruit and naM-caP are at the same spot.
 	 * 
@@ -428,7 +196,7 @@ public class NamCap {
 	 * @param x
 	 * @param y
 	 * @param grid
-	 * @return
+	 * @return 
 	 */
     public Coordinate getKiwiPath(int x, int y, Map grid) {
 
@@ -535,6 +303,14 @@ public class NamCap {
         return((x >= 0 && x < grid.getLength()) && (y >= 0 && y < grid.getRowLength(x)) && (grid.get(x, y) == '.' || grid.get(x, y) == 'k')); 
     }
     
+    /**
+     * 
+     * @param x
+     * @param y
+     * @param grid
+     * @param player
+     * @return
+     */
     public boolean isFree2(int x, int y, Map grid, Player player) {
     	//System.out.println("x: " + x + ", y: " + y + ", playerx: " + player.getX() / 30 + ", playery: " + player.getY() / 30);
         return((x >= 0 && x < grid.getLength()) && (y >= 0 && y < grid.getRowLength(x)) && (grid.get(x, y) == '.'  || grid.get(x, y) == 'k' || (x == player.getX() && y == player.getY()))); 
